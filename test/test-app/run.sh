c8ada426392121f8dc19592edf9308edff9b96dc
mshukla-intel <mangalx.shukla@intel.com>
2021-08-12 22:20:29 +0530

    Update run.sh

--- test/test-app/run.sh
+++ test/test-app/run.sh
@@ -9,4 +9,4 @@ source /opt/intel/openvino_2021/bin/setupvars.sh && \
     -m_reid ./open_model_zoo-2021.4/demos/crossroad_camera_demo/cpp/intel/person-reidentification-retail-0287/FP16-INT8/person-reidentification-retail-0287.xml \
     -d CPU \
     -no_show \
-    -o /opt/results/cpp-test-result-210812-2.mp4
+    -o /data/cpp-test-result-210812-2.mp4
