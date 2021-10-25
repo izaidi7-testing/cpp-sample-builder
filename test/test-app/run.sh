483524c729e3bcca147fb0be2b060475b5d5b4df
izaidi7-testing <69081109+izaidi7-testing@users.noreply.github.com>
2021-10-25 13:48:05 +0530

    Update run.sh

--- test/test-app/run.sh
+++ test/test-app/run.sh
@@ -9,4 +9,4 @@ source /opt/intel/openvino_2021/bin/setupvars.sh && \
     -m_reid ./open_model_zoo-2021.4/demos/crossroad_camera_demo/cpp/intel/person-reidentification-retail-0287/FP16-INT8/person-reidentification-retail-0287.xml \
     -d GPU \
     -no_show \
-    -o /data/cpp-test-result-210813.mp4
+    -o /data/output_%03d.jpg
